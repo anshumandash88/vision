9b6487894b307ad43e911fe23876817cb111bc22
anshumandash88 <anshumandash88@gmail.com>
2016-01-30 00:25:05 +0100

    Integrated code. Facae detection + Google Reverse Image Search Object detection

---
+++ app/src/main/java/miuc/stg/com/camera/RetrieveInformation.java
@@ -0,0 +1,67 @@
+package miuc.stg.com.camera;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by J on 17/11/2015.
+ */
+public class RetrieveInformation {
+
+    public String getLink(String HTML){
+        Document doc = parseHTML(HTML);
+        Element element = parseLink(doc);
+
+        return element.attr("href");
+    }
+
+    public String getTag(String HTML) throws JSONException {
+        Document doc = parseHTML(HTML);
+        Element element = parseImage(doc);
+        JSONObject obj = new JSONObject(element.text());
+
+        String s = obj.getString("s");
+        String pattern = "<b>(.*?)</b>";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(s);
+
+        System.out.println(s);
+
+        if (m.find( )) {
+            System.out.println("Found value: " + m.group(1) );
+            s = m.group(1);
+        } else {
+            System.out.println("NO MATCH");
+        }
+
+        return s;
+    }
+
+    private Element parseImage(Document doc){
+        Element element = doc.getElementById("rg_s");
+        element = element.child(0);
+        Elements elements = element.getElementsByClass("rg_meta");
+        element = elements.first();
+
+        return element;
+    }
+
+    private Element parseLink(Document doc){
+        Element element = doc.getElementById("imagebox_bigimages");
+        Elements elements = element.getElementsByTag("a");
+        element = elements.first();
+
+        return element;
+    }
+
+    private Document parseHTML(String HTML){
+        return Jsoup.parse(HTML);
+    }
+}
